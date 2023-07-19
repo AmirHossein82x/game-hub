640c81f1db889abbf2313b9e669621a2a98dfb38
amir hossein <amirhosseing983@gmail.com>
2023-07-19 14:47:29 +0330

    clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,13 +14,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="30px">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <Platformiconlist
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></Platformiconlist>
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
+        <Heading fontSize="25px">{game.name}</Heading>
       </CardBody>
     </Card>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -21,9 +21,9 @@ const GameGrid = ({gameQuery}: Props) => {
     <>
       <Text>{error && <p>{error}</p>}</Text>
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={10}
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((item) => (
